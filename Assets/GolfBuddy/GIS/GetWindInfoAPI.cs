af1d8b1796d6d82a021f7fea8f174ae83a940fad
meca_p <dudrjs7171@gmail.com>
2023-03-02 08:05:17 +0900

    Update GetWindInfoAPI.cs

--- Assets/GolfBuddy/GIS/GetWindInfoAPI.cs
+++ Assets/GolfBuddy/GIS/GetWindInfoAPI.cs
@@ -11,7 +11,7 @@ public class GetWindInfoAPI
     void Start()
     {
         url = "http://apis.data.go.kr/6260000/BusanWindInfoService/getWindInfo"; // URL
-        url += "?ServiceKey=" + "bfVQ6nT8H8ip3wr7sA9c%2BwQawshXDZyBa%2FYpEZTtVAZhjhIdAgYvD6KZUbVpGGdR74kJ6leRDHyO0uVINGJo2w%3D%3D"; // Service Key
+        url += "?ServiceKey=" + "************"; // Service Key 제거했음
         url += "&pageNo=1";
         url += "&numOfRows=5";
         url += "&resultType=json";
